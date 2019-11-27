01cdd346903b164d6cc56327aa6f7b2c69c69f88
Zachary Cowan <cz8ch8ry@gmail.com>
2019-11-27 13:56:03 -0500

    Initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Chat App</h1>
+      <button>Create Room</button>
+      <button>Join Room</button>
+    </div>
+  );
+};
+
+export default Home;
